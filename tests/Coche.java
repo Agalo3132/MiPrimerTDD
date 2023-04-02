54946010d3fd0f33935310b926265f3403e4d2ad
Alvaro <alvarosermar38@gmail.com>
2023-04-02 20:44:34 +0200

    Rama Refactorizada

--- tests/Coche.java
+++ tests/Coche.java
@@ -1,11 +1,11 @@
 public class Coche {
     public int velocidad;
 
-    public void acelerar(int aceleracion) {
+    public void acelerar_AlvaroSerrano(int aceleracion) {
         velocidad += aceleracion;
     }
 
-    public void decelerar(int deceleracion) {
+    public void decelerar_AlvaroSerrano(int deceleracion) {
         velocidad -= deceleracion;
         if (velocidad < 0) velocidad = 0;
     }
